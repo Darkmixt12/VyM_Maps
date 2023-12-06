000c9203b1c24d27ca07e18254c134411ba31c47
Steven Muñoz <darkmixt@gmail.com>
2023-12-06 15:54:13 -0600

    feat display btn locations on map

--- src/app/maps/components/map-view/map-view.component.ts
+++ src/app/maps/components/map-view/map-view.component.ts
@@ -1,92 +1,90 @@
-import { Component, OnInit, inject, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  inject,
+  AfterViewInit,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
 import { MapService, PlacesService } from '../../services';
 import { Map, Popup, Marker, LngLat } from 'mapbox-gl';
 import * as mapboxgl from 'mapbox-gl';
 import { RenderLocation } from 'src/app/home/pages/descripciones-mapas/descripciones-mapas.component';
-
-
+import { elementAt } from 'rxjs';
 
 @Component({
   selector: 'app-map-view',
   templateUrl: './map-view.component.html',
-  styleUrls: ['./map-view.component.css']
+  styleUrls: ['./map-view.component.css'],
 })
-export class MapViewComponent implements OnInit, AfterViewInit{
-  
-  private _placesService = inject(PlacesService)
-  private _mapService = inject(MapService)
+export class MapViewComponent implements OnInit, AfterViewInit {
+  private _placesService = inject(PlacesService);
+  private _mapService = inject(MapService);
   public map?: Map;
-  public ubicacion! : LngLat
+  public ubicacion!: LngLat;
 
-  public lugaresRender? : RenderLocation[]
+  public lugaresRender?: RenderLocation[];
 
   @ViewChild('mapDiv')
-  mapDivElement!: ElementRef
-
-  
-
-
-
-
+  mapDivElement!: ElementRef;
 
   ngOnInit() {
-    console.log( this._placesService.userLocation)
-    this.readFormLocalStorage()
-
+    this.readFormLocalStorage();
   }
 
-
   ngAfterViewInit(): void {
-
-    if(!this._placesService.userLocation) throw new Error('No hay placesService.userLocation')
+    if (!this._placesService.userLocation)
+      throw new Error('No hay placesService.userLocation');
 
     this.map = new Map({
       container: this.mapDivElement.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: this._placesService.userLocation, // starting position [lng, lat]
       zoom: 14, // starting zoom
-      });
+    });
 
-
-    const popup = new Popup()
-        .setHTML(`
+    const popup = new Popup().setHTML(`
           <h6>Aqui estoy</h6>
           <span>Estoy en este lugar del Mundo</span>
         `);
 
-    new Marker({color: 'red'})
-      .setLngLat( this._placesService.userLocation) 
-      .setPopup( popup )  
-      .addTo( this.map ) 
+    new Marker({ color: 'red' })
+      .setLngLat(this._placesService.userLocation)
+      .setPopup(popup)
+      .addTo(this.map);
 
-    this._mapService.setMap(this.map)
-
-
+    this._mapService.setMap(this.map);
   }
 
-  readFormLocalStorage(){
-    const plainMarkersString = localStorage.getItem('locations') ?? '[]'
-    const plainMarkers = JSON.parse(plainMarkersString)
-    this.lugaresRender = plainMarkers
+  readFormLocalStorage() {
+    const plainMarkersString = localStorage.getItem('locations') ?? '[]';
+    const plainMarkers = JSON.parse(plainMarkersString);
+    this.lugaresRender = plainMarkers;
   }
 
-  
-  generateMarkers(){
+  marksPorProvincia(provincias?: string, color?: string) {
+    if (!this.map) return;
+    this.lugaresRender?.forEach(({ lngLat, provincia }) => {
+    
+      if (provincia === provincias) {
+        console.log(this.lugaresRender)
+        new Marker({
+          color: color,
+          draggable: false,
+        })
+          .setLngLat(lngLat)
+          .addTo(this.map!);
 
-    if(!this.map) return
-
-    this.lugaresRender?.forEach( ({lngLat, provincia}) => {
-      const [lng, lat] = lngLat
-       const coords = new LngLat( lng, lat);
-
-     new Marker({
-      color: '#075e62',
-      draggable: false
-     })
-     .setLngLat(coords)
-     .addTo(this.map!)
-    })
+      } else {
+        if (provincias === undefined) {
+          new Marker({
+            color: '#075e62',
+            draggable: false,
+          })
+            .setLngLat(lngLat)
+            .addTo(this.map!);
+        }
+      }
+    });
   }
-
-
 }
