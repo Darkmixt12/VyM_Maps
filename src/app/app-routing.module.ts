12e8f5eb5b0f6e637664336db9a28e75841b400e
Steven Muñoz <darkmixt@gmail.com>
2024-01-03 16:49:51 -0600

    feat: isnotauthenticated guard added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MapPageComponent } from './maps/pages/map-page/map-page.component';
 import { isAuthenticatedGuard } from './auth/guards/is-authenticated.guard';
+import { isNotAuthenticatedGuard } from './auth/guards/is-not-authenticated.guard';
 
 const routes: Routes = [
   {
     path: 'auth', 
+    canActivate: [isNotAuthenticatedGuard],
     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
   },
   {
--- src/app/auth/guards/is-not-authenticated.guard.ts
+++ src/app/auth/guards/is-not-authenticated.guard.ts
@@ -3,7 +3,7 @@ import { AuthService } from '../services/auth.service';
 import { inject } from '@angular/core';
 import { AuthStatus } from '../interfaces';
 
-export const isAuthenticatedGuard: CanActivateFn = (route, state) => {
+export const isNotAuthenticatedGuard: CanActivateFn = (route, state) => {
 
   // const url = state.url
   // localStorage.setItem('url', url)
@@ -15,13 +15,9 @@ export const isAuthenticatedGuard: CanActivateFn = (route, state) => {
   const router = inject( Router );
   
   if( authService.authStatus() === AuthStatus.authenticated){
-    return true;
+    router.navigateByUrl('/home/inicio')
+    return false;
   }
 
-  // if( authService.authStatus() === AuthStatus.checking){
-  //   return false;
-  // }
-
-  router.navigateByUrl('/auth/login')
-  return false;
+  return true ;
 };
