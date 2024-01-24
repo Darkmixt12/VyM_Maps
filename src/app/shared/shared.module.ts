7b74fe29a502de73a2d2f54a4864cc211d6b4424
Steven Muñoz <darkmixt@gmail.com>
2024-01-24 17:53:10 -0600

    feat: file up component added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,12 +3,14 @@ import { NavBarComponent } from './navBar/nav-bar/nav-bar.component';
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
 import { SidebarModule } from 'primeng/sidebar';
+import { UploadBtnComponent } from './upload-btn/upload-btn.component';
 
 
 @NgModule({
 	declarations: [
   
-    NavBarComponent
+    NavBarComponent,
+        UploadBtnComponent
   ],
 	imports: [
 		CommonModule,
@@ -16,7 +18,8 @@ import { SidebarModule } from 'primeng/sidebar';
 		SidebarModule
 	],
 	exports:[
-		NavBarComponent
+		NavBarComponent,
+		UploadBtnComponent
 	]
   })
   export class SharedModule { }
---
+++ src/app/shared/upload-btn/upload-btn.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-btn',
+  templateUrl: './upload-btn.component.html',
+  styleUrls: ['./upload-btn.component.css']
+})
+export class UploadBtnComponent {
+
+}
